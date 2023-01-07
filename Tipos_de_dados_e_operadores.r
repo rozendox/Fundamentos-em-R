a60961711cb47ea878dd8dff535327f8cbfbc84d
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-01-07 15:54:56 -0300

    Create Tipos_de_dados_e_operadores.r

---
+++ Tipos_de_dados_e_operadores.r
@@ -0,0 +1,48 @@
+> #declaração implícita numérica
+> 
+> delta = 8
+> class(delta)
+[1] "numeric"
+> 
+> #L declara como inteniro
+> 
+> delta = 8L
+> class(delta)
+[1] "integer"
+> 
+> #outros tipos
+> Logico = TRUE
+> logico = F
+> caractere1 = "Texto"
+> caractere2 = 'Texto'
+> 
+> class(Logico)
+[1] "logical"
+> class(logico)
+[1] "logical"
+> class(caractere1)
+[1] "character"
+> class(caractere2)
+[1] "character"
+> 
+> #Operações Matemáticas
+> c = 2
+> d = 4
+> (c + d) * d
+[1] 24
+> e = (c + d ) * d
+> e
+[1] 24
+> #raiz quadrada
+> sqrt(2500)
+[1] 50
+> 
+> 
+> #testes logicos
+> a = 1
+> b = 2
+> a < b
+[1] TRUE
+> a == b
+[1] FALSE
+> 
